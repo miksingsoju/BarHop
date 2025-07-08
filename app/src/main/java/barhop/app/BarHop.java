7894aaad187225473a9890f4ec04e14a92eef1f6
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-07-08 16:43:31 +0800

    Added Realms

---
+++ app/src/main/java/barhop/app/BarHop.java
@@ -0,0 +1,15 @@
+package barhop.app;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class BarHop extends Application {
+
+    public void onCreate()
+    {
+        super.onCreate();
+
+        Realm.init(this);
+    }
+}
