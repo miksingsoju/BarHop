a1443a0e6a6c21c09f9aa58743ef9493bf770733
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-07-15 01:58:27 +0800

    Updated the Main Activity
    - When you login, the login button will disappear, and should ideally be replaced by a logout button (not implemented yet)
    - isUserLoggedIn() State added

--- app/src/main/java/barhop/app/actvity/MainActivity.java
+++ app/src/main/java/barhop/app/actvity/MainActivity.java
@@ -13,6 +13,10 @@ import barhop.app.R;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import barhop.app.model.User;
+import io.realm.Realm;
 
 /**
  * This is the first thing the user sees when opening the app (even without logging in)
@@ -30,15 +34,24 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         init();
+        isUserLoggedIn();
     }
 
+    Realm realm;
+
     Button loginButton;
+    TextView mainText;
+
 
     /**
      * This method initializes the views needed.
      */
     public void init(){
+        realm = Realm.getDefaultInstance();
+
         loginButton = findViewById(R.id.loginButton);
+        mainText = findViewById(R.id.mainText);
+
         loginButton.setOnClickListener(view -> login());
 
     }
@@ -49,6 +62,20 @@ public class MainActivity extends AppCompatActivity {
     private void login(){
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
+    }
+
+    private void isUserLoggedIn() {
+        String userUuid = getIntent().getStringExtra("uuid");
+
+        if (userUuid != null) {
+            // Fetch user from Realm using UUID
+            User user = realm.where(User.class).equalTo("uuid", userUuid).findFirst();
+
+            if (user != null) {
+                mainText.setText("Welcome, " + user.getDisplayName() + "!");
+                loginButton.setVisibility(View.GONE); // hide login button
+            }
+        }
     }
 
 
