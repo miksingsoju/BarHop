40de74e15a5c8807b867d9c9c8a2d904557a03c6
neons-amiel <robertamiel.abadilla@gmail.com>
2025-07-15 02:05:02 +0800

    Declared Variables in CreateBar
    Add Bar Button nalang hehe

--- app/src/main/java/barhop/app/actvity/CreateBar.java
+++ app/src/main/java/barhop/app/actvity/CreateBar.java
@@ -8,9 +8,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import android.content.Intent;
+
+
+
 import barhop.app.R;
 
 public class CreateBar extends AppCompatActivity {
+
+    Button createBarReturnButton, createBarAddButton;
+    TextView createBarLabel, createBarPicLabel, createBarNameLabel, createBarAddressLabel, createBarDescriptionLabel;
+    EditText createBarNameField, createBarAddressField, createBarDescriptionField;
+    ImageButton createBarImageField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +37,41 @@ public class CreateBar extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
     }
+
+    public void initViews() {
+        createBarLabel = findViewById(R.id.CreateBarLabel);
+        createBarPicLabel = findViewById(R.id.createBarPicLabel);
+        createBarNameLabel = findViewById(R.id.createBarNameLabel);
+        createBarAddressLabel = findViewById(R.id.createBarAddressLabel);
+        createBarDescriptionLabel = findViewById(R.id.createBarDescriptionLabel);
+
+        createBarNameField = findViewById(R.id.createBarNameField);
+        createBarAddressField = findViewById(R.id.createBarAddressField);
+        createBarDescriptionField = findViewById(R.id.createBarDescriptionField);
+
+        createBarImageField = findViewById(R.id.createBarImageField);
+
+        createBarReturnButton = findViewById(R.id.createBarReturnButton);
+        createBarAddButton = findViewById(R.id.createBarAddButton);
+
+        createBarReturnButton.setOnClickListener(v -> ReturnLanding());
+        createBarAddButton.setOnClickListener(v -> AddBar());
+
+    }
+
+    public void ReturnLanding() {
+        Intent intent = new Intent(this, BarList.class);
+        startActivity(intent);
+
+    }
+
+    public void AddBar() {
+        // Please do add bar stuff here
+
+
+    }
+
+
 }
